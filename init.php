01bb4adacefb5f48c7b104afb35cff2023992a66
unknown <yacine.djellouah.coursdoranco2@gmail.com>
2022-11-17 15:00:18 +0100

    première initialisation

---
+++ init.php
@@ -0,0 +1,9 @@
+<?php
+
+/** ############################### GIT################################################
+ * 4 status de fichier sur git
+ *  - untracked => les fichiers créés mais non suivis par git
+ *  - tracked   => les fichiers suivis par git mais non envoyés sur le repository
+ *  - staged    => les fichiers suivis enregistrés mais non envoyés vers le repository
+ *  - commited   => les fichiers envoyés et non modifiés du projet
+ */
